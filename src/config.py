b03fcde5a17d337b9815dd3fa48da770b468e3a7
unknown <hansotto.kristiansen@gmail.com>
2023-08-30 10:45:19 +0200

    protocol fixes

--- src/config.py
+++ src/config.py
@@ -17,22 +17,22 @@ class Config:
     
 class DevelopmentConfig(Config):
     DEBUG = True
-    FRONTEND_PORT = ':9000'
-    FRONTEND_URL = 'localhost'
     FRONTEND_PROTOCOL = 'http://'
+    FRONTEND_URL = 'localhost'
+    FRONTEND_PORT = ':9000'
 
+    BACKEND_PROTOCOL = 'http://'
     BACKEND_URL = 'localhost'
     BACKEND_PORT = ':5000'
-    BACKEND_PROTOCOL = 'http://'
 
 class ProductionConfig(Config):
     DEBUG = False
-    FRONTEND_PROTOCOL = 'https'
+    FRONTEND_PROTOCOL = 'https://'
     FRONTEND_URL = 'qp-invest-frontend.web.app'
-    FRONTEND_PORT = 'https://'
+    FRONTEND_PORT = ''
     
-    BACKEND_URL = "qpinvest-nbdj-main-i4enuayvva-lz.a.run.app"
     BACKEND_PROTOCOL = 'https://'
+    BACKEND_URL = "qpinvest-nbdj-main-i4enuayvva-lz.a.run.app"
     BACKEND_PORT = ''
 
 # Function to get the selected configuration based on env_node
